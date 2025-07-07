a262c9c48a3c55d71471b5a8e5a2b68b38281a4b
yazhini25-eng <119488692+yazhini25-eng@users.noreply.github.com>
2025-07-07 20:43:45 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,10 @@
+public class evenorodd {
+    public static void main(String[] args){
+        int num=20;
+        if(num%2==0){
+            System.out.print("even");
+        }else{
+            System.out.print("odd");
+        }
+    }
+}
---
+++ greatestofthreenumbers.java
@@ -0,0 +1,17 @@
+public class greatestofthreenumbers {
+    public static void main(String[] args){
+        int a=56;
+        int b=10;
+        int c=25;
+        if(a>=b && a>=c){
+            System.out.print("a is graeter");
+        }
+        else if(b>=a && b>=c){
+            System.out.print("b is graeter");
+        }
+        else{
+            System.out.print("c is graeter");
+        }
+    }
+    
+}
